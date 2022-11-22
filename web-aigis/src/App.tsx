c579696337226f9fe1f46579a6b7b4dff21fbf90
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2022-11-23 02:41:10 +0900

    他のデータを選べるようにする

--- web-aigis/src/App.tsx
+++ web-aigis/src/App.tsx
@@ -4,20 +4,39 @@ import { BufferGeometry, BufferGeometryLoader, Material, Mesh } from "three";
 import loadJson from "./loadJson";
 import { useEffect, useRef, useState } from "react";
 import { ColorMap } from "./types/loader";
+import Select from "react-select";
+
+const itokawaDataOptions = [
+  {
+    value: "itokawaAcceleration",
+    label: "Itokawa Acceleration",
+  },
+  {
+    value: "itokawaGeopotentialHeight",
+    label: "Itokawa Geopotential Height",
+  },
+  {
+    value: "itokawaGravitationalSlope",
+    label: "Itokawa Gravitational Slope",
+  },
+];
 
 export function App() {
   const [geometry, setGeometry] = useState<BufferGeometry | null>(null);
   const [material, setMaterial] = useState<Material | null>(null);
   const [isPointerDown, setIsPointerDown] = useState(false);
   const [colorMap, setColorMap] = useState<ColorMap>(ColorMap.Rainbow);
+  const [dataOption, setDataOption] = useState(itokawaDataOptions[0]);
   const ref = useRef<Mesh>(null);
   const loader = new BufferGeometryLoader();
   useEffect(() => {
     loader.load(
-      import.meta.env.PROD ? "../test.json" : "../public/test.json",
+      import.meta.env.PROD
+        ? `../${dataOption.value}.json`
+        : `../public/${dataOption.value}.json`,
       (geometry) => loadJson(geometry, setGeometry, setMaterial, colorMap)
     );
-  }, [colorMap]);
+  }, [colorMap, dataOption]);
 
   const reverseColorMap =
     colorMap === ColorMap.Rainbow ? ColorMap.Grayscale : ColorMap.Rainbow;
@@ -47,12 +66,19 @@ export function App() {
         <ambientLight intensity={0.1} />
         <directionalLight color="white" position={[0, 0, 5]} />
       </Canvas>
+
       <button
         onClick={() => setColorMap(reverseColorMap)}
         className="color-map-button"
       >
         change color
       </button>
+      <Select
+        className="data-selector"
+        options={itokawaDataOptions}
+        value={dataOption}
+        onChange={(option) => option && setDataOption(option)}
+      />
     </div>
   );
 }
